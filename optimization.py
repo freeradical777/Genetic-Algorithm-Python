c742b7d71356b54a43be9f586d1c41d39f05c235
Jacob Bender <benderjacob44@gmail.com>
2016-01-17 18:59:31 -0500

    Commiting initial changes

--- optimization.py
+++ optimization.py
@@ -1,4 +1,12 @@
-import random
+import random, math
 
-orcs = []
+orcs = [(1,1)]
 
+def eval_orc(orc):
+    try:
+        out=orc[0]/orc[1]
+        score=abs(math.pi-out)
+    except(ZeroDivisionError):
+        score=9001 #It's over 9000
+    return score
+    
