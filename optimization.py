128b1d51f595ff77bf5df1c8f10b047963cd4544
Christopher King <G.nius.ck@gmail.com>
2016-01-17 18:58:51 -0500

    Added mutate

--- optimization.py
+++ optimization.py
@@ -2,3 +2,17 @@ import random
 
 orcs = [(1,1)]
 
+def mutate(orc):
+    x=random.randrange(5)
+    num = orc[0]
+    den = orc[1]
+    if x == 0:
+        return orc
+    elif x==1:
+        return (orc[0]+1,orc[1])
+    elif x==2:
+        return (orc[0]-1,orc[1])
+    elif x==3:
+        return (orc[0],orc[1]+1)
+    elif x==4:
+        return (orc[0],orc[1]-1)
