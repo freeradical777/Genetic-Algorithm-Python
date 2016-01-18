da8604bd106c7e59ba8d1e66abb8db05aed106a5
Christopher King <G.nius.ck@gmail.com>
2016-01-17 20:59:00 -0500

    Alphabetization

--- optimization.py
+++ optimization.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 Jacob Bender and Christopher King
+# Copyright (C) 2016 Christopher King and Jacob Bender
 
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
