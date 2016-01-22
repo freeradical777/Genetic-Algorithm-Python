dcdcefd31bf7e3411869b13917fe0ab237283ba1
Jacob Bender <benderjacob44@gmail.com>
2016-01-21 19:27:47 -0500

    Fixed error in population function. Wrote the evaluation and filter functions

--- strings.py
+++ strings.py
@@ -1,4 +1,4 @@
-#   Copyright (C) 2016 Jacob Bender
+#   Copyright (C) 2016 Jacob Bender and Christopher King
 
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -16,14 +16,28 @@
 import random, string
 
 orgs=[]
-tolerance = 500 #number of orgs to keep
+tolerance = 5 #number of orgs to keep
 letters=string.lowercase #letters to choose from
 target="abc" #the target string
 
 def pop_orgs(pop_size): #Populate number of orgs
-    for i in xrange(pop_size)
+    for i in xrange(pop_size):
         new_org=""
         for x in xrange(len(target)):
-            new_org=new_org + random.choice(letters)
+            new_org=new_org + letters[random.randrange(26)]
         orgs.append(new_org)
+
+def eval_org(org):
+    score=len(target)
+    for x in xrange(len(target)):
+        if target[x]==org[x]:
+            score-=1
+    return score
+
+def filter_orgs():
+    global orgs
+    
+    orgs.sort(key=eval_org)
+
+    orgs=orgs[:tolerance]                
 
